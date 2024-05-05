b78f51335f09d911811feb6094f17dccc97d6894
Mykola Gurov <ngurov@gmail.com>
2024-05-05 21:34:50 +0200

    client site navigation worked

--- app/routes/fetched.$seq.tsx
+++ app/routes/fetched.$seq.tsx
@@ -1,3 +1,7 @@
+import { useParams } from "@remix-run/react";
+
 export default function NumberFetcher() {
-    return <>Something to be here I guess</>;
+    const params = useParams();
+
+    return <>Something to be here I guess, seq # {params['seq']}</>;
 }
